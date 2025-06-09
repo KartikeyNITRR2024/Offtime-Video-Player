e9fd25f712111ff0775cb5f5f49ca0ce04cb0efa
Kartikey2001nitrr <kartikeythawait123@gmail.com>
2025-06-10 03:50:24 +0530

    With websocket

--- src/main/java/com/offtime/videoplayer/config/WebSocketConfig.java
+++ src/main/java/com/offtime/videoplayer/config/WebSocketConfig.java
@@ -3,7 +3,6 @@ package com.offtime.videoplayer.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
@@ -12,16 +11,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Value("${frontend.urls}")
     private String[] allowedOrigins;
-    
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic", "/queue"); 
+        config.enableSimpleBroker("/topic", "/queue");
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns(allowedOrigins).withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins(allowedOrigins)
+                .withSockJS()
+                .setSessionCookieNeeded(true);
     }
-
 }
